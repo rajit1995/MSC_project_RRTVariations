ce5e9e256bb65b2cca91602bbb6f71422ed0f270
Rajit Roy <83898887+rajit1995@users.noreply.github.com>
2023-09-28 10:37:09 +0100

    Add files via upload

---
+++ RRT Algorithms with Rover Models/Basic RRT/getqnew.m
@@ -0,0 +1,8 @@
+function RRTState = getqnew(RRTState)
+    if norm(RRTState.q_near - RRTState.q_new) <= RRTState.StepSize
+        RRTState.q_new = RRTState.q_new;
+    else
+        RRTState.q_new = RRTState.q_near + 2*RRTState.StepSize*(RRTState.q_new - RRTState.q_near)/norm((RRTState.q_new - RRTState.q_near));
+        
+    end   
+end
